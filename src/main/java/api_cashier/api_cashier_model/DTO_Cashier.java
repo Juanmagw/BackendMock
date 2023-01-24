28f05f71e2f036513b91b155f74a7c5a2e0f8a41
JoseCP82 <josecamarap@gmail.com>
2023-01-24 18:44:24 +0100

    DTO_Cashier

--- src/main/java/api_cashier/api_cashier_model/DTO_Cashier.java
+++ src/main/java/api_cashier/api_cashier_model/DTO_Cashier.java
@@ -1,4 +1,38 @@
 package api_cashier.api_cashier_model;
 
-public class DTO_Cashier {
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="cashier")
+public class DTO_Cashier implements Serializable {
+
+    private static final long serialVersion = 1L;
+
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name="photo")
+    private Byte[] photo;
+    @Column(name="address")
+    private String address;
+    @Column(name="cp")
+    private Integer cp;
+    @Column(name="lattitude")
+    private Double lattitude;
+    @Column(name="longitude")
+    private Double longitude;
+    @Column(name="balance")
+    private Double balance;
+    @Column(name="available")
+    private Boolean available;
+    @JsonBackReference
+    @ManyToMany(cascade = CascadeType.REMOVE, fetch = FetchType.LAZY)
+    @JoinTable(name=)
+
+
 }
